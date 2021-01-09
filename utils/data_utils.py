fbcb7a13373becb578165a7784e19e4ad081066a
Osama Afzal <osama.bscs17seecs@seecs.edu.pk>
2021-01-09 16:23:34 +0300

    Updated for Custom Dataset

--- utils/data_utils.py
+++ utils/data_utils.py
@@ -1,13 +1,18 @@
 import logging
-
 import torch
-
 from torchvision import transforms, datasets
-from torch.utils.data import DataLoader, RandomSampler, DistributedSampler, SequentialSampler
+from torch.utils.data import DataLoader, RandomSampler, DistributedSampler, SequentialSampler, Subset
+from sklearn.model_selection import train_test_split
 
 
 logger = logging.getLogger(__name__)
 
+def train_val_dataset(dataset, val_split=0.25):
+    train_idx, val_idx = train_test_split(list(range(len(dataset))), test_size=val_split)
+    datasets = {}
+    datasets['train'] = Subset(dataset, train_idx)
+    datasets['val'] = Subset(dataset, val_idx)
+    return datasets
 
 def get_loader(args):
     if args.local_rank not in [-1, 0]:
@@ -18,36 +23,16 @@ def get_loader(args):
         transforms.ToTensor(),
         transforms.Normalize(mean=[0.5, 0.5, 0.5], std=[0.5, 0.5, 0.5]),
     ])
-    transform_test = transforms.Compose([
-        transforms.Resize((args.img_size, args.img_size)),
-        transforms.ToTensor(),
-        transforms.Normalize(mean=[0.5, 0.5, 0.5], std=[0.5, 0.5, 0.5]),
-    ])
 
-    if args.dataset == "cifar10":
-        trainset = datasets.CIFAR10(root="./data",
-                                    train=True,
-                                    download=True,
-                                    transform=transform_train)
-        testset = datasets.CIFAR10(root="./data",
-                                   train=False,
-                                   download=True,
-                                   transform=transform_test) if args.local_rank in [-1, 0] else None
+    dataset = datasets.ImageFolder('dataset/train/', transform=transform_train)
+    trainset,testset = train_val_dataset(dataset)
 
-    else:
-        trainset = datasets.CIFAR100(root="./data",
-                                     train=True,
-                                     download=True,
-                                     transform=transform_train)
-        testset = datasets.CIFAR100(root="./data",
-                                    train=False,
-                                    download=True,
-                                    transform=transform_test) if args.local_rank in [-1, 0] else None
     if args.local_rank == 0:
         torch.distributed.barrier()
 
     train_sampler = RandomSampler(trainset) if args.local_rank == -1 else DistributedSampler(trainset)
     test_sampler = SequentialSampler(testset)
+
     train_loader = DataLoader(trainset,
                               sampler=train_sampler,
                               batch_size=args.train_batch_size,
